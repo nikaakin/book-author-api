256005192801b8bdd56dbcbbea2838857fc511eb
nikaakin <nikacuckiridze47@gmail.com>
2024-02-29 16:02:15 +0400

    refactor: modify route names

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index'
 Route::group(['controller' => BookController::class, 'prefix' => 'books',], function () {
     Route::get('/', 'index')->name('books.index');
     Route::get('/{book}', 'show')->name('books.show');
-    Route::post('/store', 'store')->name('books.store');
-    Route::patch('/update/{book}', 'update')->name('books.update');
-    Route::delete('/destroy/{book}', 'destroy')->name('books.destroy');
+    Route::post('/', 'store')->name('books.store');
+    Route::patch('/{book}', 'update')->name('books.update');
+    Route::delete('/{book}', 'destroy')->name('books.destroy');
 });
